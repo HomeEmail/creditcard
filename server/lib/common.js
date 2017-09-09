5951c370acc15e25323d12722e86d1a6971c0f89
ivan <591694604@qq.com>
2017-09-09 23:56:41 +0800

    add common

--- server/lib/common.js
+++ server/lib/common.js
@@ -1,16 +1,45 @@
 //公共基础工具库
+var Hashes = require('jshashes');//加密库
+var uuidv1 = require('uuid/v1');//base timestamp
+var uuidv4 = require('uuid/v4');//base random
+var uuidv5 = require('uuid/v5');//base namespace
 
 module.exports = {
-    mysql: {
-        host: '127.0.0.1', 
-        user: 'root',
-        password: '',
-        database:'creditcard', // 数据库
-        port: 3306
+    sha1:function(s){
+        return new Hashes.SHA1().hex(s);
     },
-    redis:{
-        host: '127.0.0.1',
-        port: 6379,
-        auth_pass: '123456'
+    sha256:function(s){
+        return new Hashes.SHA256().hex(s);
+    },
+    sha512:function(s){
+        return new Hashes.SHA512().hex(s);
+    },
+    md5:function(s){
+        return new Hashes.MD5().hex(s);
+    },
+    uuidv1 : function(){
+        return uuidv1();
+    },
+    uuidv4 : function(){
+        return uuidv4();
+    },
+    uuidv5 : function(s,type){
+        if(type=='DNS'){
+            return uuidv5(s, uuidv5.DNS); 
+        }
+        if(type=='URL'){
+            return uuidv5(s,uuidv5.URL);
+        }
+        var MY_NAMESPACE = '3a602f20-956f-11e7-99ce-17399588291f';
+        return uuidv5(s, MY_NAMESPACE);
     }
 };
+
+
+
+
+
+
+
+
+
