9b7bee29f16e9b39ae362888e345dfdd0f251668
ivan <591694604@qq.com>
2017-11-03 21:40:33 +0800

    add ftp

--- server/lib/ftp_test.js
+++ server/lib/ftp_test.js
@@ -11,49 +11,43 @@ c.on('ready', function() {
 		if (err) throw err;
 		//console.dir(list);
 		var len=list.length;
-		var buffer;
-		list.forEach(function (element, index, array) {
-			//buffer = new Buffer(element.name,'utf8');
+		var element=null;
+		//var buffer;
+		for(var index=0;index<len;index++){
+			element=list[i];
 			//element.name=iconv.decode(element.name,'gb2312');
-			//下载，文件名有中文不行，
-            //Ignore directories
-            if (element.type === 'd') {
-                console.log('ignoring directory ' + element.name);
-                return;
-            }
-            //Ignore non zips
-            if (path.extname(element.name) == '.zip') {
-                console.log('ignoring file ' + element.name);
-                return;
-            }
-            if(element.name.indexOf('wap live')>-1){ //eclipse问题汇总 wap live hybrid
-            	console.log('begin Download file-->');
-            	c.get('/tvutvgo/'+element.name, function (err, stream) {
-                	if (err) throw err;
-	                stream.once('close', function () {
-	                	console.log('Download File OK!');
-	                    c.end();
+			//上传下载，文件名不能有中文，
 
-	                });
-	                stream.pipe(fs.createWriteStream(element.name));//下载到当前终端执行命令的目录下
-	            });
-            }else{
-            	console.log('no this file! ->'+element.name);
-            	// if(index==(len-1)){//遍历到最后一个文件了
-	            // 	c.end();
-	            // }
-            }
-            
+			//Ignore directories
+			if (element.type === 'd') {
+				console.log('ignoring directory ' + element.name);
+				return;
+			}
+			//Ignore non zips
+			if (path.extname(element.name) == '.zip') {
+				console.log('ignoring file ' + element.name);
+				return;
+			}
+			if(element.name.indexOf('wap live')>-1){ //eclipse问题汇总 wap live hybrid
+				console.log('begin Download file-->');
+				c.get('/tvutvgo/'+element.name, function (err, stream) {
+					if (err) throw err;
+					stream.once('close', function () {
+						console.log('Download File OK!');
+						c.end();
 
-            //Download files
-            /*c.get(element.name, function (err, stream) {
-                if (err) throw err;
-                stream.once('close', function () {
-                    c.end();
-                });
-                stream.pipe(fs.createWriteStream(element.name));
-            });*/
-        });
+					});
+					stream.pipe(fs.createWriteStream(element.name));//下载到当前终端执行命令的目录下
+				});
+				break;
+			}else{
+				console.log('no this file! ->'+element.name);
+				 if(index==(len-1)){//遍历到最后一个文件了
+				 	c.end();
+				 }
+			}
+		}
+
 		//c.end();
 	});
 });
