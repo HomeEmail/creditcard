89cbf406031f2611f75c753989ed91090adbd580
HomeEmail <591694604@qq.com>
2017-11-06 15:30:56 +0800

    add jsftp
    Signed-off-by: HomeEmail <591694604@qq.com>

--- server/lib/ftp_test.js
+++ server/lib/ftp_test.js
@@ -4,8 +4,28 @@ var config=require('../conf/ftp');
 var path = require('path');
 var iconv = require('iconv-lite');
 
+var JSFtp = require('jsftp');//另外一个ftp库，中文也会乱码
+var jsftp= new JSFtp({
+	host: "192.168.4.21",
+	port: 21, // defaults to 21 
+	user: "ivan", // defaults to "anonymous" 
+	pass: "123456" // defaults to "@anonymous" 
+});
+
+// jsftp.ls(".", function(err, res) {
+//   res.forEach(function(file) {
+//     console.log(file.name);
+//   });
+// });
+
+// jsftp.list("./tvutvgo", function(err, res) {
+// 	console.log(res);
+	
+// });
+
+
 //Get a directory listing of the current (remote) working directory:
-var c = new ftp();
+/*var c = new ftp();
 c.on('ready', function() {
 	c.list('/tvutvgo/',function(err, list) {
 		if (err) throw err;
@@ -53,7 +73,7 @@ c.on('ready', function() {
 });
 // connect to localhost:21 as anonymous 
 c.connect(config);
-
+*/
 
 /*//Download remote file 'foo.txt' and save it to the local file system:
 var c = new ftp();
